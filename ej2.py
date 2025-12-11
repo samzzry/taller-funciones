4a99704a44a9cdf2a729d464d0e15ed5dd625091
samzzry <samuelhumanez6@gmail.com>
2025-12-11 17:47:53 -0500

    ej2.py

--- ej2.py
+++ ej2.py
@@ -1,32 +1,34 @@
 def pedir_codigo():
-    codigo = input("Ingrese su código de identificación: ")
+    codigo = input("ingrese su codigo de identificacion ")
     return codigo
-
 def validar_codigo(codigo, codigo_especial):
     if codigo == codigo_especial:
         return True
     else:
         return False
-
 def procesar_accesos():
-    codigo_especial = "INV-001"
+    codigo_especial = "1234"
     permitidos = 0
     denegados = 0
-
     while True:
         cod = pedir_codigo()
-        if cod.upper() == "SALIR":
+        if cod == "salir":
             break
-
         if validar_codigo(cod, codigo_especial):
-            print("Acceso permitido.")
-            permitidos += 1
+            print("accceso permitido")
+            permitidos = permitidos + 1
         else:
             print("Acceso denegado.")
-            denegados += 1
-    
+            denegados = denegados + 1
     return permitidos, denegados
-
 def mostrar_resultados(permitidos, denegados):
-    print(f"Accesos permitidos: {permitidos}")
-    print(f"Accesos denegados: {denegados}")
+    print("acceso permitido", permitidos)
+    print("Acceso denegado", denegados)
+
+
+
+
+
+// PROGRAMA PRINCIPAL
+permitidos, denegados = procesar_accesos()
+mostrar_resultados(permitidos, denegados)

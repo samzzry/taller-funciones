bed1b78a65d965de11c28779c487a031eecd57e1
samzzry <samuelhumanez6@gmail.com>
2025-12-11 18:10:53 -0500

    ej6.py

--- ej6.py
+++ ej6.py
@@ -1,24 +1,23 @@
 def pedir_cpu():
-    c = float(input("Uso de CPU (%): "))
+    c = float(input("uso de cpu "))
     return c
-
 def procesar_cpu():
     alertas = 0
     mediciones = 0
-
     uso = pedir_cpu()
-
     while uso >= 0:
         mediciones = mediciones + 1
-
         if uso > 90:
-            print("Alerta: Uso Crítico")
+            print("alerta uso critico")
             alertas = alertas + 1
-
         uso = pedir_cpu()
-
     return alertas, mediciones
 
 def mostrar_cpu(alertas, mediciones):
-    print("Total de mediciones:", mediciones)
-    print("Alertas críticas:", alertas)
+    print("total de medicione", mediciones)
+    print("alerta critica", alertas)
+
+
+// programa principal
+a, m = procesar_cpu()
+mostrar_cpu(a, m)

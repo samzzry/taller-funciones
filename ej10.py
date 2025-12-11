3b3a91a29249fbb34e4ce6b42c7811e7e5cb7c12
samzzry <samuelhumanez6@gmail.com>
2025-12-11 17:49:34 -0500

    ej10.py

--- ej10.py
+++ ej10.py
@@ -1,28 +1,25 @@
-def pedir_horas():
+def pedir():
     h = int(input("Horas extra trabajadas: "))
     return h
-
-def procesar_horas():
-    total_bono = 0
-    empleados_bono = 0
-
-    horas = pedir_horas()
-
+def procesar():
+    total = 0
+    emp = 0
+    horas = pedir()
     while horas >= 0:
         if horas > 5:
             bono = horas * 15
-            total_bono = total_bono + bono
-            empleados_bono = empleados_bono + 1
+            total = total + bono
+            emp = emp + 1
         else:
             if horas > 0:
                 bono = horas * 10
-                total_bono = total_bono + bono
-                empleados_bono = empleados_bono + 1
-
-        horas = pedir_horas()
-
-    return total_bono, empleados_bono
-
-def mostrar_bonos(total_bono, empleados_bono):
-    print("Bonificación total:", total_bono)
-    print("Empleados con bonificación:", empleados_bono)
+                total = total + bono
+                emp = emp + 1
+        horas = pedir()
+    return total, emp
+def mostrar(total, emp):
+    print("Bonificacion total:", total)
+    print("Empleados con bonificacion:", emp)
+// PROGRAMA PRINCIPAL
+total, emp = procesar()
+mostrar(total, emp)

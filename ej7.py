092f47ef5fde196bec17312269ca0cf0f6af6906
samzzry <samuelhumanez6@gmail.com>
2025-12-11 18:14:16 -0500

    ej7.py

--- ej7.py
+++ ej7.py
@@ -1,25 +1,22 @@
 def pedir_venta():
-    v = float(input("Monto de venta del vendedor: "))
+    v = float(input("monto de venta del vendedor"))
     return v
-
 def procesar_ventas():
     meta = 5000
     cumplidos = 0
-    total_vendedores = 0
-
+    total = 0
     venta = pedir_venta()
-
     while venta > 0:
-        total_vendedores = total_vendedores + 1
-
+        total = total + 1
         if venta >= meta:
             cumplidos = cumplidos + 1
-            print("Meta cumplida")
-
+            print("meta cumplida")
         venta = pedir_venta()
+    return cumplidos, total
+def mostrar_ventas(cumplidos, total):
+    print("vendedores con meta cumplida", cumplidos)
+    print("total de vendedores procesados", total)
 
-    return cumplidos, total_vendedores
-
-def mostrar_ventas(cumplidos, total_vendedores):
-    print("Vendedores con meta cumplida:", cumplidos)
-    print("Total de vendedores procesados:", total_vendedores)
+// programa principal
+c, t = procesar_ventas()
+mostrar_ventas(c, t)

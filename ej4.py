b2ad7e9b36d2e5c2558d86c58da36a9c8135b38d
samzzry <samuelhumanez6@gmail.com>
2025-12-11 18:02:27 -0500

    ej4.py

--- ej4.py
+++ ej4.py
@@ -1,18 +1,15 @@
 def pedir_unidades():
-    u = int(input("Cantidad de unidades del lote: "))
+    u = int(input("cantidad de unidades del lote "))
     return u
-
 def pedir_estado():
-    e = input("Estado (D defectuosa / O ok): ")
+    e = input("estado (D defectuosa / O ok) ")
     return e
-
-def procesar_lotes():
+def procesar():
     defectuosas = 0
     ok = 0
+    seguir = input("Escriba stop para terminar o enter para continuar: ")
 
-    seguir = input("Escriba STOP para terminar o ENTER para continuar: ")
-
-    while seguir != "STOP":
+    while seguir != "stop":
         unidades = pedir_unidades()
 
         contador = 0
@@ -30,11 +27,11 @@ def procesar_lotes():
 
             contador = contador + 1
 
-        seguir = input("Escriba STOP para terminar o ENTER para continuar: ")
+        seguir = input("escriba  stop para terminar o ente para continuar: ")
 
     return defectuosas, ok
 
-def mostrar_resumen(defectuosas, ok):
+def mostrar(defectuosas, ok):
     total = defectuosas + ok
 
     if total > 0:
@@ -42,6 +39,10 @@ def mostrar_resumen(defectuosas, ok):
     else:
         porcentaje = 0
 
-    print("Defectuosas:", defectuosas)
-    print("OK:", ok)
-    print("Porcentaje defectuosas:", porcentaje)
+    print("defectuosa", defectuosas)
+    print("ok:", ok)
+    print("porcentaje defectuosa ", porcentaje)
+
+// programa principal
+d, o = procesar()
+mostrar(d, o)
